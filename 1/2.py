5468bcb1a7425a72bf3b5dab44b4fb37bd5fda22
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-19 12:00:38 +0530

    Average Salary Excluding the Minimum and Maximum Salary

---
+++ 1/2.py
@@ -0,0 +1,6 @@
+# 1491
+# https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        return sum(sorted(salary)[1:-1])/(len(salary)-2)
