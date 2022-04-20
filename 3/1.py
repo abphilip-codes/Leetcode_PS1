c6671ae742c318fc44ebf573d33bdaf23082f54f
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-20 11:38:05 +0530

    README

---
+++ 3/1.py
@@ -0,0 +1,9 @@
+# 976
+# https://leetcode.com/problems/largest-perimeter-triangle/
+
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        for z in reversed(range(len(nums)-2)):
+            if(nums[z]+nums[z+1]>nums[z+2]): return sum(nums[z:z+3])
+        return 0
---
+++ 3/2.py
@@ -0,0 +1,11 @@
+# 1779
+# https://leetcode.com/problems/find-nearest-point-that-has-the-same-x-or-y-coordinate/
+
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        s, p = 0, 1
+        while(n>0):
+            s+=n%10
+            p*=n%10
+            n//=10
+        return p-s
