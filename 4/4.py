03cd9393a5ad6187feed0bcd066ba048e4f291ad
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-21 11:53:02 +0530

    Check if One String Swap Can Make Strings Equal

--- 4/4.py
+++ 4/4.py
@@ -1,11 +1,11 @@
-# 1779
-# https://leetcode.com/problems/find-nearest-point-that-has-the-same-x-or-y-coordinate/
+# 1790
+# https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/
 
 class Solution:
-    def nearestValidPoint(self, x: int, y: int, points: List[List[int]]) -> int:
-        m, ans = math.inf, -1
-        for z in range(len(points)):
-            if(points[z][0]!=x and points[z][1]!=y): continue
-            d = abs(points[z][0]-x)+abs(points[z][1]-y)
-            if(m>d): m, ans = d, z
-        return ans
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        a, b = [], []
+        for z in range(len(s1)):
+            if(s1[z]!=s2[z]):
+                a.insert(0, s1[z])
+                b.append(s2[z])
+        return (s1==s2 or (len(a)==2 and a==b))
