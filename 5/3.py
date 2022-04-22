53e3cf76c74b69356234fcb47c0eac5047aa8a60
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-22 11:11:08 +0530

    Check If It Is a Straight Line

--- 5/3.py
+++ 5/3.py
@@ -1,15 +1,10 @@
-# 202
-# https://leetcode.com/problems/happy-number/
+# 1232
+# https://leetcode.com/problems/check-if-it-is-a-straight-line/
 
 class Solution:
-    def isHappy(self, n: int) -> bool:
-        s=set()
-        while(True):
-            k=0
-            while(n):
-                k+=(n%10)**2
-                n//=10
-            if k in s: return False
-            if(k==1): return True
-            s.add(k)
-            n=k
+    def checkStraightLine(self, c: List[List[int]]) -> bool:
+        dx, dy = c[0][0]-c[1][0], c[0][1]-c[1][1]
+        for z in range(1,len(c)-1):
+            if((c[z][0]-c[z+1][0])*dy !=  
+            (c[z][1]-c[z+1][1])*dx): return False
+        return True
