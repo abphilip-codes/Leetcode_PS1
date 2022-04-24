f638a133108afb0f195a0622ee93fa7f9023e12f
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-24 13:39:37 +0530

    Richest Customer Wealth

--- 6/3.py
+++ 6/3.py
@@ -1,10 +1,6 @@
-# 1232
-# https://leetcode.com/problems/check-if-it-is-a-straight-line/
+# 1672
+# https://leetcode.com/problems/richest-customer-wealth/
 
 class Solution:
-    def checkStraightLine(self, c: List[List[int]]) -> bool:
-        dx, dy = c[0][0]-c[1][0], c[0][1]-c[1][1]
-        for z in range(1,len(c)-1):
-            if((c[z][0]-c[z+1][0])*dy !=  
-            (c[z][1]-c[z+1][1])*dx): return False
-        return True
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max(sum(a) for a in accounts)
