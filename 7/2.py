91d90a7a92abe1f77e90ea624b3e5bdc70484279
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-24 18:18:13 +0530

    Reshape the Matrix

--- 7/2.py
+++ 7/2.py
@@ -3,7 +3,7 @@
 
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
-        m, n = len(mat), len(mat[0])
-        if m * n != r * c: return mat
-        arr = [mat[row][col] for row in range(m) for col in range(n)]
-        return [arr[i:i + c] for i in range(0, r * c, c)]
+        x, y = len(mat), len(mat[0])
+        if(x*y!=r*c): return mat
+        arr = [mat[a][b] for a in range(x) for b in range(y)]
+        return [arr[k:k+c] for k in range(0, r*c, c)]
