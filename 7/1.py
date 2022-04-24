fac47ef96e11ad3957fff990efc543814a6af430
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-24 16:18:28 +0530

    Matrix Diagonal Sum

--- 7/1.py
+++ 7/1.py
@@ -2,10 +2,9 @@
 # https://leetcode.com/problems/matrix-diagonal-sum/
 
 class Solution:
-    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        ans=0
-        for z in range(len(arr)+1):
-            for y in range(z):
-                if(len(arr[y:z])%2!=0):
-                    ans+=sum(arr[y:z])
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        ans, n = 0, len(mat)
+        for z in range(n):
+            ans += (mat[z][z] + mat[z][-1-z])
+        if(n%2): ans -= (mat[n//2][n//2])
         return ans
