c6228ce7b4d589984e9e3a873856c3a60816692b
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-26 11:40:33 +0530

    To Lower Case

--- 9/1.py
+++ 9/1.py
@@ -1,8 +1,6 @@
-# 1768
-# https://leetcode.com/problems/merge-strings-alternately/
+# 709
+# https://leetcode.com/problems/to-lower-case/
 
 class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        l = min(len(word1), len(word2))
-        k = [word1[z] + word2[z] for z in range(l)]
-        return "".join(k) + word1[l:] + word2[l:]
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
