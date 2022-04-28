b2f47e26b790ed679bd58098808fb30df49f824f
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-28 07:54:57 +0530

    Implement Queue using Stacks

--- XI/2.py
+++ XI/2.py
@@ -1,16 +1,38 @@
 # 232
 # https://leetcode.com/problems/implement-queue-using-stacks/
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class MyQueue:
 
-class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        s, f = head, head
-        while(f and f.next):
-            s = s.next
-            f = f.next.next
-        return s
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        self.s1.append(x)
+
+    def pop(self) -> int:
+        for z in range(len(self.s1)-1):
+            self.s2.append(self.s1.pop())
+        ans = self.s1.pop()
+        for z in range(len(self.s1)-1):
+            self.s1.append(self.s2.pop())
+        return ans
+
+    def peek(self) -> int:
+        for z in range(len(self.s1)-1):
+            self.s2.append(self.s1.pop())
+        ans = self.s1[0]
+        for z in range(len(self.s1)-1):
+            self.s1.append(self.s2.pop())
+        return ans
+
+    def empty(self) -> bool:
+        return len(self.s1)==0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
