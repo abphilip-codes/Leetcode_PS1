fba0990f7f341b67f944f51b4bfb84d20c8946f2
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-28 07:47:33 +0530

    Sort Integers by The Number of 1 Bits

--- XI/1.py
+++ XI/1.py
@@ -1,21 +1,6 @@
 # 1356
 # https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
 class Solution:
-    def getDecimalValue(self, head: ListNode) -> int:
-        if(head.val==1 and not head.next): return int(1)
-        else:
-            ans, h = '', head
-            while(h):
-                ans += str(h.val)
-                h = h.next
-            s, l = 0, len(ans)
-            for z in range(l-1,-1,-1):
-                s += (2**z)*int(ans[l-1-z])
-            return s
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        return sorted(arr, key = lambda x: (bin(x).count('1'), x))
