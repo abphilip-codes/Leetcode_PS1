955580dcec0a977c4a055b2918093f64d6d38e31
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-28 08:03:05 +0530

    Contains Duplicate

--- XI/4.py
+++ XI/4.py
@@ -1,19 +1,6 @@
 # 217
 # https://leetcode.com/problems/contains-duplicate/
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Solution:
-    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        def f(k,ans,p):
-            if(not k): return 0
-            if(not k.left and not k.right and p==1):
-                ans+=k.val
-                return ans
-            return(f(k.left,ans,1) + f(k.right,ans,0))
-        return f(root,0,0)
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums)!=len(set(nums))
