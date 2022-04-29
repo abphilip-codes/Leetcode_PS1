a137dfee657e1eead10e71ebd1f534ffaa9dcc4a
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-29 14:48:43 +0530

    Design Parking System

--- XII/1.py
+++ XII/1.py
@@ -1,6 +1,18 @@
 # 1603
 # https://leetcode.com/problems/design-parking-system/
 
-class Solution:
-    def sortByBits(self, arr: List[int]) -> List[int]:
-        return sorted(arr, key = lambda x: (bin(x).count('1'), x))
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.a = {1: big, 2: medium, 3: small}
+
+    def addCar(self, carType: int) -> bool:
+        if(self.a[carType]>0):
+            self.a[carType]-=1
+            return True
+        return False
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
