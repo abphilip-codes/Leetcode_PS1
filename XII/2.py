c5ad9bb7aa744a63886ebf29ddf6383383650b58
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-29 14:51:39 +0530

    Range Sum Query - Immutable

--- XII/2.py
+++ XII/2.py
@@ -1,38 +1,15 @@
 # 303
 # https://leetcode.com/problems/range-sum-query-immutable/
 
-class MyQueue:
+class NumArray:
 
-    def __init__(self):
-        self.s1 = []
-        self.s2 = []
+    def __init__(self, nums: List[int]):
+        self.a = list(accumulate(nums))
 
-    def push(self, x: int) -> None:
-        self.s1.append(x)
-
-    def pop(self) -> int:
-        for z in range(len(self.s1)-1):
-            self.s2.append(self.s1.pop())
-        ans = self.s1.pop()
-        for z in range(len(self.s1)-1):
-            self.s1.append(self.s2.pop())
-        return ans
-
-    def peek(self) -> int:
-        for z in range(len(self.s1)-1):
-            self.s2.append(self.s1.pop())
-        ans = self.s1[0]
-        for z in range(len(self.s1)-1):
-            self.s1.append(self.s2.pop())
-        return ans
-
-    def empty(self) -> bool:
-        return len(self.s1)==0
+    def sumRange(self, left: int, right: int) -> int:
+        return(self.a[right]-(self.a[left-1] if(left) else 0)) 
 
 
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
